ad09abd1dceddbfd0efccd541c6e739feb028962
Gautam Sharma <108661866+SHIVANSHBH@users.noreply.github.com>
2026-07-09 10:23:55 +0530

    Time: 23 ms (30.32%), Space: 59.7 MB (36.9%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,15 @@
+class RecentCounter {
+    private java.util.Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new java.util.LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
